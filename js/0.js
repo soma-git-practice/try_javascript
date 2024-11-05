6c1965e828f365d01c22565b03867ccbfce67ace
soma <somofgit@gmail.com>
2024-11-06 08:56:38 +0900

    24/11/06

--- js/0.js
+++ js/0.js
@@ -1,3 +1,11 @@
 "use strict";
 
-alert('hello')
+// 12が帰ってくることを期待していた。
+const max = 10
+const num = function add(val) {  
+  val += 3;
+  if (val < max) { add(val) };
+  val
+}
+
+console.log(num(1));
