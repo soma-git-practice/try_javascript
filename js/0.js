70657f104a13ef24389a4c7d88804decd85ed562
soma <somofgit@gmail.com>
2024-11-18 23:09:35 +0900

    11/18

--- js/0.js
+++ js/0.js
@@ -1,17 +1,37 @@
 "use strict";
 console.clear();
 
-function counter(initial_num) {
-  return {
-    total: initial_num,
-    up: function () {
-      this.total++;
-    }
-  };
+// コンストラクタ関数
+function User(name) {
+  this.name = name;
+};
+
+// コンストラクタ関数を宣言するとprototypeプロパティが追加されていることの確認。
+console.log(User.prototype); // => {}
+
+// prototypeプロパティのintroductionプロパティに関数オブジェクトを設定する。
+User.prototype.introduction = function () {
+  console.log(`はじめまして！SomOfGitさん、私の名前は${this.name}です。`);
+};
+
+// インスタンス化
+const instance = new User('太郎');
+
+// インスタンスの__proto__プロパティとコンストラクタのprototypeプロパティの値は同じ場所を参照している。
+console.log(instance.__proto__ === User.prototype);
+
+
+// -----------------
+
+// クラスもprototypeプロパティを持っている
+class Hoge {
+  constructor(name) {
+    this.name = name;
+  }
+
+  introduction() {
+    console.log(`My name is ${this.name}`);
+  }
 }
 
-const raijousha_counter = counter(2);
-raijousha_counter.up()
-console.log(raijousha_counter.total);
-raijousha_counter.up()
-console.log(raijousha_counter.total);
+console.log(Hoge.prototype.introduction); // # => function(){`My name is ${this.name}`}
